608c27ef75a384a6833226ab43b84ae726eff4b3
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-03-04 04:44:12 -0500

    hamburger menu structure

--- src/assets/components/Header/Header.js
+++ src/assets/components/Header/Header.js
@@ -1,9 +1,42 @@
-import "./Header.scss"
+import "./Header.scss";
+import Icons from "../IconHolder/IconHolder";
+import { useState } from "react";
 
-export default function Header (){
-    return(
-        <>
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
 
-        </>
-    )
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="header">
+      {isOpen ? (
+        <menu className="menu">
+          <button className="menu-button" onClick={handleMenu}>
+            <img
+              src={Icons().CloseIcon}
+              alt="close menu"
+              className="menu-button__icon"
+            />
+          </button>
+          <ul className="nav">
+            <li className="nav__link">About</li>
+            <li className="nav__link">Portfolio</li>
+            <li className="nav__link">Contact</li>
+          </ul>
+        </menu>
+      ) : (
+        <menu className="menu">
+            <button className="menu-button" onClick={handleMenu}>
+            <img
+                src={Icons().MenuIcon}
+                alt="hamburger menu"
+                className="menu-button__icon"
+            />
+            </button>
+        </menu>
+      )}
+    </header>
+  );
 }
---
+++ src/assets/components/IconHolder/IconHolder.js
@@ -0,0 +1,14 @@
+import MenuIcon from "../../icons/menu_fill.svg";
+import CloseIcon from "../../icons/close_fill.svg";
+
+
+function Icons() {
+    const icons = {
+        MenuIcon: MenuIcon,
+        CloseIcon: CloseIcon,
+    };
+
+    return icons;
+}
+
+export default Icons;
