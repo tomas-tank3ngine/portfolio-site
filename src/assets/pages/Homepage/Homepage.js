4813043ff7a0a7a430ea325dff5eeb156009722f
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-03-05 00:21:24 -0500

    add homepage html structure

--- src/assets/pages/Homepage/Homepage.js
+++ src/assets/pages/Homepage/Homepage.js
@@ -1,5 +1,82 @@
 import "./Homepage.scss";
+import portrait from "../../images/Tomas Portrait.png";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
-  return <>Hello</>;
+  return (
+    <main className="main">
+      <section className="about">
+        <div className="about__headshot">
+          <img
+            src={portrait}
+            alt="portrait"
+            className="about__headshot--image"
+          />
+        </div>
+        <h2 className="about__header">
+          Hi there 👋 I'm Tomas, A Full-Stack Software Developer
+        </h2>
+        <p className="about__bio">
+          I'm also a self-taught programmer, bootcamp grad, entrepreneur, and
+          former escape room designer. I've always been someone who enjoys
+          finding creative solutions to complex problems especially when it
+          comes to creating compelling, high quality experiences.
+        </p>
+      </section>
+      <section className="pitch">
+        <h2 className="pitch__header">I love working with Small Businesses</h2>
+        <p className="pitch__bio">
+          I specialize in creating sites for individuals and small businesses.
+          You shouldn't have to settle for cheap solutions or generic templates.
+          I provide custom designs at affordable prices. Frequently your website
+          is the first impression your customers will get, so make sure it's a
+          good one. Take a look at my portfolio below, if you think I'd be a
+          good match send me an email.
+        </p>
+      </section>
+
+      <section className="card">
+        <h2 className="card__header">I'm a Full-Stack Web Developer</h2>
+        <p className="card__description">
+          I like to code things from scratch, and enjoy bringing ideas to life
+          in the browser.
+        </p>
+        <section className="card-info">
+          <h3 className="card-info__header">Languages I speak</h3>
+          <ul className="info-list">
+            <li className="info-list__item">HTML</li>
+            <li className="info-list__item">CSS</li>
+            <li className="info-list__item">JavaScript</li>
+            <li className="info-list__item">React</li>
+            <li className="info-list__item">Sass</li>
+            <li className="info-list__item">Git</li>
+            <li className="info-list__item">SQL</li>
+            <li className="info-list__item">TypeScript</li>
+            <li className="info-list__item">Node.js</li>
+            <li className="info-list__item">Jest</li>
+          </ul>
+        </section>
+        <section className="card-info">
+          <h3 className="card-info__header">Dev Tools</h3>
+          <ul className="info-list">
+            <li className="info-list__item">VSCode</li>
+            <li className="info-list__item">Heroku</li>
+            <li className="info-list__item">Netlify</li>
+            <li className="info-list__item">Github</li>
+          </ul>
+        </section>
+      </section>
+      <section className="recent-work">
+        <h2 className="recent-work__header">My Recent Work</h2>
+        <p className="recent-work__tagline">Here are a few past designs I've worked on. Want more info? Email me</p>
+        <ul className="recent-work__list">
+            <li className="site">
+                <Link className="site__link">
+
+                </Link>
+            </li>
+        </ul>
+      </section>
+    </main>
+  );
 }
